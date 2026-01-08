9db807c94f6806ff7098f1174996004af0191eb1
LukeVassallo18 <lukevas189@gmail.com>
2026-01-08 21:31:51 +0100

    removed double BGrowserRouterComponent from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import ExpenseTracking from "./components/ExpenseTracking";
 import IncomeManagement from "./components/IncomeManagement";
@@ -7,17 +7,15 @@ import SavingsGoals from "./components/SavingsGoals";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/income" element={<IncomeManagement />} />
-          <Route path="/expenses" element={<ExpenseTracking />} />
-          <Route path="/savings" element={<SavingsGoals />} />
-        </Routes>
-        <Navigation />
-      </div>
-    </BrowserRouter>
+    <div className="min-h-screen bg-gray-50">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/expenses" element={<ExpenseTracking />} />
+        <Route path="/income" element={<IncomeManagement />} />
+        <Route path="/savings" element={<SavingsGoals />} />
+      </Routes>
+    </div>
   );
 }
 
